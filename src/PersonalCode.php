d161852a4733c213c2ea6dc0c2f7e638f8a988fb
Laurynas Gavienas <laurynas.gavienas@bigbank.lt>
2015-05-07 15:12:30 +0300

    WIP

--- src/PersonalCode.php
+++ src/PersonalCode.php
@@ -13,12 +13,33 @@ class PersonalCode implements PersonalCodeInterface
 {
 
     /**
+     * Gender constants.
+     */
+    const GENDER_MALE = 'male';
+    const GENDER_FEMALE = 'female';
+    const GENDER_UNKNOWN = 'unknown';
+
+    /**
+     * @var array Array of digits that are used to designate a male.
+     */
+    protected $maleFirstDigits = [1, 3, 5];
+
+    /**
+     * @var array Array of digits that are used to designate a female.
+     */
+    protected $femaleFirstDigit = [2, 4, 6];
+
+    /**
      * @var int
      */
     private $personalCode;
 
-    public function __construct( $personalCode )
+    /**
+     * @param int $personalCode
+     */
+    public function __construct($personalCode)
     {
+
         $this->personalCode = $personalCode;
     }
 
@@ -28,5 +49,61 @@ class PersonalCode implements PersonalCodeInterface
     public function getId()
     {
         return $this->personalCode;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getGender()
+    {
+
+        if ($this->isMale()) {
+            return self::GENDER_MALE;
+        }
+        if ($this->isFemale()) {
+            return self::GENDER_FEMALE;
+        }
+
+        return self::GENDER_UNKNOWN;
+    }
+
+    /**
+     * Checks if this personal code belongs to male.
+     *
+     * @return bool
+     */
+    public function isMale()
+    {
+
+        return in_array($this->getFirstDigit(), $this->maleFirstDigits);
+    }
+
+    /**
+     * Checks if this personal code belongs to female.
+     *
+     * @return bool
+     */
+    public function isFemale()
+    {
+
+        return in_array($this->getFirstDigit(), $this->femaleFirstDigit);
+    }
+
+    public function getBirthday()
+    {
+
+        $firstDigit = $this->getFirstDigit();
+
+    }
+
+    /**
+     * Returns first digit of the personal code.
+     *
+     * @return int
+     */
+    private function getFirstDigit()
+    {
+
+        return (int) substr($this->personalCode, 0, 1);
     }
 }
--- src/PersonalCodeInterface.php
+++ src/PersonalCodeInterface.php
@@ -2,12 +2,36 @@
 
 namespace EProjects\EstonianPersonalCode;
 
+/**
+ * Interface for defining personal code object
+ */
 interface PersonalCodeInterface
 {
     /**
-     * Returns personal code
+     * Returns personal code.
      *
      * @return int
      */
     public function getId();
+
+    /**
+     * Extracts gender from personal code.
+     *
+     * @return string
+     */
+    public function getGender();
+
+    /**
+     * Checks if this personal code belongs to male.
+     *
+     * @return bool
+     */
+    public function isMale();
+
+    /**
+     * Checks if this personal code belongs to female.
+     *
+     * @return bool
+     */
+    public function isFemale();
 }
